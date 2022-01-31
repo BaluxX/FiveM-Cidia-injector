4cabbb0413074828173fcc91f871f1072c1d7411
Master Dream <98493659+Master-Dream404@users.noreply.github.com>
2022-01-31 21:19:02 +0100

    Update offsets.h

--- old Cidia injector/Cidia injector/offsets.h
+++ old Cidia injector/Cidia injector/offsets.h
@@ -4,8 +4,8 @@
 namespace offset
 {
 	std::uint64_t ResourceCore = 0x6EFDD; //30/01/2022
-	std::uint64_t loadfile = 0xB8110;
+	std::uint64_t loadfile = 0xB8110; //30/01/2022
 
-	static uint64_t bypassAddress = 0x16480; //0x16350
+	static uint64_t bypassAddress = 0x16480; //29/01/2022
 	static uint64_t dlcmountcount = 0x69; // Maybe if it gives too many errors we should write to the offset manually :)
 }
